4acc0343ad1657e64b3b513392f64fba8b90c7ad
JC <ulatjc@gmail.com>
2021-03-26 22:07:46 -0700

    fixed variables; edit format

--- producer.py
+++ producer.py
@@ -1,6 +1,6 @@
 """
 The main function of this program is to send generated data (data.py) to a Kakfa producer
-running on 'SOME.HOST.IP:9092' and print out a copy of the data to the terminal.
+running on 'KAFKA_HOST_IP:9092' and print out a copy of the data to the terminal.
 """
 
 import json
@@ -9,6 +9,10 @@ import random
 import csv
 from data import get_row
 from kafka import KafkaProducer
+
+# Replace kafka variables 
+#TOPIC=TOPIC
+#KAFKA_HOST_IP=KAFKA_HOST_IP
 
 def json_serializer(data):
     """
@@ -20,7 +24,7 @@ def json_serializer(data):
 
 # Instantiate Kafka Producer 
 producer = KafkaProducer(
-    bootstrap_servers = ['<SOME.HOST.IP>:9092'], # bootstrap-server-ip:9092   
+    bootstrap_servers = [f'{KAFKA_HOST_IP}:9092'], # ie. bootstrap-server-ip:9092   
     value_serializer=json_serializer # 
 ) 
 
@@ -29,6 +33,6 @@ if __name__ == "__main__":
         row = get_row()
         for r in row:
             print(r)
-            producer.send("{TOPIC}", r) # TOPIC to send messages to  
+            producer.send(f"{TOPIC}", r) # Kafka topic  
             random_int= random.uniform(1, 3)
             time.sleep(random_int)
